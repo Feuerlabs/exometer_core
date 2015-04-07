cc08af865807a47ecc0288e7f0dd539ac4f8c18f
Ulf Wiger <ulf@feuerlabs.com>
2015-04-07 15:27:54 +0200

    fix copyright notices

--- test/exometer_report_SUITE.erl
+++ test/exometer_report_SUITE.erl
@@ -1,14 +1,14 @@
--module(exometer_report_SUITE).
-
 %% -------------------------------------------------------------------
 %%
-%% Copyright (c) 2014 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2015 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %%   This Source Code Form is subject to the terms of the Mozilla Public
 %%   License, v. 2.0. If a copy of the MPL was not distributed with this
 %%   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %%
 %% -------------------------------------------------------------------
+-module(exometer_report_SUITE).
+
 %% common_test exports
 -export(
    [
--- test/exometer_test_udp_reporter.erl
+++ test/exometer_test_udp_reporter.erl
@@ -1,3 +1,12 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2015 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%%   This Source Code Form is subject to the terms of the Mozilla Public
+%%   License, v. 2.0. If a copy of the MPL was not distributed with this
+%%   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+%%
+%% -------------------------------------------------------------------
 -module(exometer_test_udp_reporter).
 
 -behaviour(exometer_report).
