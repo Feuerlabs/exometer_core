9e4e4256e71e313070996a9d8e3fc4e270820f4e
Bartłomiej Górny <b.gorny@in-genero.com>
2016-04-04 17:21:17 +0200

    fixed type specs

--- src/exometer.erl
+++ src/exometer.erl
@@ -76,7 +76,11 @@
 -include("log.hrl").
 
 -type name()        :: list().
--type type()        :: atom().
+-type type()        :: atom()
+                     | {function, M :: atom(), F :: atom()}
+                     | {function, M :: atom(), F :: atom(), ArgSpec :: list(),
+                        Type :: atom(), DataPoints :: list()}
+                     | {Type :: atom(), Arg :: any()}.
 -type status()      :: enabled | disabled.
 -type options()     :: [{atom(), any()}].
 -type value()       :: any().
